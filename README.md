d52d32ad3a26fd47432b37959ab7d6c01fd1f19c
char <25884226+char@users.noreply.github.com>
2018-10-31 22:15:02 -0700

    Add EU support thanks to pooky(friend) and save variants to file

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ The result of the article [Anatomy of a Supreme Bot](https://medium.com/@.com/an
 
 To run this:
 `npm install`
-`node .`
+If you're from the US:
+`npm run us`
+If you're from EU:
+`npm run eu`
 
-All done.
+All done. Variants saves to output.json
 
 Probably best to find variants a day or some hours before a drop. Haven't tested it in longer durations.
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 const axiosRetry = require('axios-retry');
- 
+const fs = require('fs');
+
 axiosRetry(axios, { retries: 3 });
 
 //mobile user-agent header for checkout.json endpoint
@@ -12,13 +13,13 @@ const options = {
 
 const getCheckoutData = (largestSize, amount) => {
     let items = {};
-    for(let i = largestSize; i < largestSize + amount; i++) {
+    for(let i = largestSize + 1; i < largestSize + amount; i++) {
         items[i] = 1;
     }
 
     const cooksub = encodeURIComponent(JSON.stringify(items).replace(/'/g, '"'));
 
-    return {
+    const usCheckoutData = {
         'store_credit_id': '',
         'from_mobile': '1',
         'cookie-sub': cooksub,
@@ -37,26 +38,39 @@ const getCheckoutData = (largestSize, amount) => {
         'credit_card[year]': '2020',
         'credit_card[rsusr]': '302',
         'order[terms]': '1'
-    }
+    };
+
+    return usCheckoutData; 
 }
 
 //get the last item id from latest drop (largest id)
-const getLargestItemID = async () => {
+const getLargestItemID = async (region) => {
     const res = await axios.get('https://supremenewyork.com/mobile_stock.json');
     const data = await res.data;
-    const newProducts = data['products_and_categories']['new'];
+    const products_and_categories = data['products_and_categories'];
 
-    let largestID = -1;
-    for(let product of newProducts) {
-        if(product.id > largestID)
-            largestID = product.id;
+    let largestProductID = 0;
+
+    if(region === 'us') {
+        largestProductID = products_and_categories['new'].map(p => p.id).reduce((a, b) => Math.max(a, b))
+        largestProductID = await getLargestSizeID(largestProductID);
+        console.log(`Largest size ID updated to: ${largestProductID}`);
+    } else {
+        for (let category in products_and_categories) {
+            for (let item of products_and_categories[category]) {
+                let sizeId = await getLargestSizeID(item['id']);
+                if (sizeId > largestProductID) {
+                    console.log(`Largest size ID updated to: ${sizeId}`);
+                    largestProductID = sizeId;
+                }
+            }
+        }
     }
     
-    console.log(`Largest ID: ${largestID}`);
-    return largestID;
-}
+    return largestProductID;
+}   
 
-//get biggest size id
+//get largest size id
 const getLargestSizeID = async (largestItemID) => {
     const res = await axios.get(`https://supremenewyork.com/shop/${largestItemID}.json`);
     const data = await res.data;
@@ -64,31 +78,54 @@ const getLargestSizeID = async (largestItemID) => {
     const lastProduct = data.styles[data.styles.length - 1]
     const lastProductID = lastProduct.sizes[lastProduct.sizes.length - 1].id;
     
-    console.log(`Largest Size ID Found: ${lastProductID}`);
     return lastProductID;
 }
 
-const fetchVariants = async (largestSizeID, amount) => {
-    const res = await axios.post('https://www.supremenewyork.com/checkout', getCheckoutData(largestSizeID, amount), options);
-    const data = await res.data;
+const fetchVariants = async (largestSizeID, region, amount) => {
+    try {
+        const res = await axios.post('https://www.supremenewyork.com/checkout.json', getCheckoutData(largestSizeID, amount), options);
+        const data = await res.data;
 
-    let variants = [];
-    if(data.status == 'outOfStock') {
-        for(let variant in data.mp) {
-            variants.push({
-                'Product Name': data.mp[variant]['Product Name'],
-                'Product Color': data.mp[variant]['Product Color'],
-                'Product Size': data.mp[variant]['Product Size'],
-                'Product ID': parseInt(largestSizeID) + parseInt(variant)
-            })
+        let variants = [];
+        if(data.status == 'outOfStock') {
+            for(let variant in data.mp) {
+                variants.push({
+                    'Product Name': data.mp[variant]['Product Name'],
+                    'Product Color': data.mp[variant]['Product Color'],
+                    'Product Size': data.mp[variant]['Product Size'],
+                    'Product ID': parseInt(largestSizeID) + parseInt(variant)
+                })
+            }
         }
+        return variants;
+    } catch(err) {
+        return [];
     }
-    return variants;
 }
 
 (async () => {
-    const largestItemID = await getLargestItemID();
-    const largestSizeID = await getLargestSizeID(largestItemID);
-    const variants = await fetchVariants(largestSizeID, 30);
-    console.log(variants.length === 0 ? 'No new variants found.' : variants);
+    const args = process.argv;
+    let region = "us";
+
+    if(args.length > 2) {
+        if(args[2].toLowerCase() === 'us' || args[2].toLowerCase() === 'eu') {
+            region = args[2].toLowerCase();
+            console.log(`Region set to ${region}`)
+        }
+    } else if(args.length <= 2) {
+        console.log('Region defaulting to US. ')
+    }
+
+    const id = await getLargestItemID(region);
+    const variants = await fetchVariants(id, region, 500);
+
+    console.log(variants.length === 0 ? 'No new variants found.' : variants.length + ' new variants have been found.')
+    if(variants.length > 0) {
+        fs.writeFile('output.json', JSON.stringify(variants,null,2), { flag: 'wx' }, (err) => {
+            if (err) 
+                return console.log(err);
+
+            console.log(`Wrote ${variants.length} variants to output.json`);
+        });
+    }
 })();
